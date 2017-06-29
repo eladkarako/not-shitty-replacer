2790da4122abcd6fa8e29c263f2ca9635bc2ed1c
Elad Karako <eladkarako@users.noreply.github.com>
2017-06-29 23:34:53 +0300

    u

--- index.cmd
+++ index.cmd
@@ -24,13 +24,15 @@ goto EXIT
 :EXIT
 
 :: for search pattern you can use any javascript regex
-::  L1xcIi9n  is  /\\\\"/g
-::  L1wiL2c=  is  /\\\"/g
-::  L2EvZw==  is  /a/g
-::  L2Ev      is  /a/
+::  L1xcIi9n            is  /\\\\"/g
+::  L1wiL2c=            is  /\\\"/g
+::  L2EvZw==            is  /a/g
+::  L2Ev                is  /a/
+::  L0ZPTlQgOCwvZw==    is  /FONT 8,/g
 
 :: for replace pattern you can use strings
-::  Jw==      is  '
-::  bW1tbQ==  is  mmmm
+::  Jw==                is  '
+::  bW1tbQ==            is  mmmm
+::  Rk9OVCAxNCw=        is  FONT 14,
 
 :: you can search /^abcd(.+)xyz$/i  and replace with $1 (escape everything to base64...)  to replace the whole string with the find.  (backtrace/backtrack)
